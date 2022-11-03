11272b41fe2bbb2f70e28828af928c19c1e8b03e
yejineee <yygeniee@gmail.com>
2022-11-03 20:34:42 +0900

    Chore: eslint 스타일 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: 'standard-with-typescript',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-  }
-}
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', {
+      arrays: 'never',
+      objects: 'always',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+    'no-useless-escape': ['warn'],
+  },
+};
