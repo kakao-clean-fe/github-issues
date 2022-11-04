bc16b8086e7514a0ba0da8f29ae6e7c974342748
yejineee <yygeniee@gmail.com>
2022-11-04 22:53:12 +0900

    Feat: getIssues 함수 작성

---
+++ src/types/issue.ts
@@ -0,0 +1,20 @@
+interface Tag {
+  tagName: string
+  color: string
+}
+
+type Status = 'open' | 'close';
+
+export interface Issue {
+  title: string
+  _id: number
+  tags: Tag[]
+  status: Status
+  'open-date': string
+  creator: string
+  projects: string
+  milestones: string
+  assignee: string
+  subtask: string[]
+  'comments-count': number
+}
