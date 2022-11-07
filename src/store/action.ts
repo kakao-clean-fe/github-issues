5037c6a93b930e5a4513873ccf79240ed82bb129
yejineee <yygeniee@gmail.com>
2022-11-07 18:23:26 +0900

    Fix: getApi에 넘기는 함수 타입을 Issue의 배열로 수정

--- src/store/action.ts
+++ src/store/action.ts
@@ -5,5 +5,5 @@ import type { Issue } from '~/types/issue';
 export const getIssues = async (): Promise<Issue[]> => {
   const PATH = '/data-sources/issues.json';
 
-  return await getApi<Issue>({ url: `${SERVER_URL}${PATH}` });
+  return await getApi<Issue[]>({ url: `${SERVER_URL}${PATH}` });
 };
