e125864ef17a9eefa3b324393919a697c183bc0a
yejineee <yygeniee@gmail.com>
2022-11-07 14:45:30 +0900

    Fix: getIssues의 리턴 타입을 Issue 배열로 수정

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,8 @@
 import { SERVER_URL } from '~/env';
 import { getApi } from '~/utils/api';
-import { Issue } from '~/types/issue';
+import type { Issue } from '~/types/issue';
 
-export const getIssues = async (): Promise<Issue> => {
+export const getIssues = async (): Promise<Issue[]> => {
   const PATH = '/data-sources/issues.json';
 
   return await getApi<Issue>({ url: `${SERVER_URL}${PATH}` });
