ec70cb75573870a2be3e509fd1bb1144666ed4d2
Sotaneum <gnyontu39@gmail.com>
2022-11-07 16:16:16 +0900

    [WEEK-1] pipe 로 적용해보기

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { getIssueItemTpl, getIssueTpl } from "./tpl";
 
+const DATA_SOURCE_ISSUES = "/data-sources/issues.json";
+
 const domParser = new DOMParser();
 
-const parser = (text) =>
-  domParser.parseFromString(text, "text/html").body.firstChild;
+const pipeStack = (funcs, value) =>
+  funcs.reduce((acc, func) => ({ ...acc, ...func(acc) }), { value });
+
+const pipe = (funcs, value) => funcs.reduce((acc, func) => func(acc), value);
+
+const parser = (text) => ({
+  target: domParser.parseFromString(text, "text/html").body.firstChild,
+});
 
 const toDict =
   (getKey = (item) => !item) =>
@@ -13,21 +21,17 @@ const toDict =
       return { ...acc, [key]: [...(acc[key] || []), item] };
     }, {});
 
-const toFetch = (path) => fetch(path).then((res) => res.json());
+const toFetch = (path, callback = (items) => items) =>
+  fetch(path)
+    .then((res) => res.json())
+    .then(callback);
 
-const renderItems =
-  (itemCreator) =>
-  (items = []) =>
-    items.map((item) => parser(itemCreator(item)));
-
-const selectElement =
-  (elementDict = {}) =>
-  (select) => {
-    Object.values(elementDict).forEach((element) =>
-      element.classList.remove("font-bold")
-    );
-    elementDict[select].classList.add("font-bold");
-  };
+const selector = (elementDict = {}, select) => {
+  Object.values(elementDict).forEach((element) =>
+    element.classList.remove("font-bold")
+  );
+  elementDict[select].classList.add("font-bold");
+};
 
 const toIssueData = toDict((item) => item.status);
 
@@ -36,35 +40,63 @@ const appendItems = (target) => (items) => {
   items.forEach((item) => target.appendChild(item));
 };
 
-const renderIssue = (target) => {
-  target.appendChild(parser(getIssueTpl()));
+const renderItems =
+  (itemCreator) =>
+  (items = []) =>
+    items.map((item) => pipe([itemCreator, parser], item).target);
 
-  const ul = target.querySelector("ul");
-  const count = {
-    open: target.querySelector(".open-count"),
-    close: target.querySelector(".close-count"),
-  };
+const renderIssueItems =
+  (items) =>
+  ({ ul, count, value }) =>
+    pipeStack(
+      [
+        (data) => ({ issueData: toIssueData(data.value) }),
+        ({ issueData }) => {
+          selector(count, value);
+          count.open.innerText = `${issueData["open"].length} Opens`;
+          count.close.innerText = `${issueData["close"].length} Closed`;
+          return {
+            issueAppend: appendItems(ul),
+            createItems: renderItems(getIssueItemTpl),
+          };
+        },
+        ({ issueData, createItems, issueAppend }) =>
+          pipe([createItems, issueAppend], issueData[value]),
+      ],
+      items
+    );
 
-  const selector = selectElement(count);
-  const issueAppend = appendItems(ul);
-  const createItems = renderItems(getIssueItemTpl);
+const loadItems = (data) =>
+  toFetch(DATA_SOURCE_ISSUES, renderIssueItems).then((callback) =>
+    callback(data)
+  );
 
-  const changeStatus = (status) =>
-    toFetch("/data-sources/issues.json")
-      .then(toIssueData)
-      .then((issueData) => {
-        selector(status);
-        count.open.innerText = `${issueData["open"].length} Opens`;
-        count.close.innerText = `${issueData["close"].length} Closed`;
-        return issueAppend(createItems(issueData[status]));
-      });
+const render = (app) =>
+  pipeStack(
+    [
+      () => pipe([parser], getIssueTpl()),
+      ({ target }) => {
+        app.appendChild(target);
 
-  count.open.addEventListener("click", () => changeStatus("open"));
-  count.close.addEventListener("click", () => changeStatus("close"));
+        return {
+          ul: target.querySelector("ul"),
+          count: {
+            open: target.querySelector(".open-count"),
+            close: target.querySelector(".close-count"),
+          },
+        };
+      },
+      ({ count, ...data }) => {
+        count.open.addEventListener("click", () =>
+          loadItems({ ...data, count, value: "open" })
+        );
+        count.close.addEventListener("click", () =>
+          loadItems({ ...data, count, value: "close" })
+        );
+      },
+      loadItems,
+    ],
+    "open"
+  );
 
-  return changeStatus;
-};
-
-const app = document.getElementById("app");
-const issue = renderIssue(app);
-issue("open");
+render(document.getElementById("app"));
