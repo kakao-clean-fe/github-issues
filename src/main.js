efb58f5662afabeca1ee04402cbfff2fa0441ed6
jordan.song <jordan.song@kakaocorp.com>
2022-11-07 17:47:47 +0900

    WEEK-1 앱 실행되지 않는 버그 수정

--- src/main.js
+++ src/main.js
@@ -11,41 +11,46 @@ import {
   removeClass,
 } from './curry/dom';
 
-const issues = await fetchIssues();
-const openedIssues = issues.filter(isOpenedIssue);
-const closedIssues = issues.filter(isClosedIssue);
+async function App() {
+  const issues = await fetchIssues();
+  const openedIssues = issues.filter(isOpenedIssue);
+  const closedIssues = issues.filter(isClosedIssue);
 
-const makeTextBold = pipe(addClass('font-bold'));
-const makeTextThin = pipe(removeClass('font-bold'));
-const renderLayout = pipe(setInnerHTML(getIssueTpl()));
-const renderOpenedIssues = pipe(
-  setInnerHTML(openedIssues.map(getIssueItemTpl).join(''))
-);
-const renderClosedIssues = pipe(
-  setInnerHTML(closedIssues.map(getIssueItemTpl).join(''))
-);
-const renderOpenedButton = pipe(
-  setInnerText(`${openedIssues.length} Opened`),
-  setEvent('click', () => {
-    makeTextBold($(sel.openedButton));
-    makeTextThin($(sel.closedButton));
+  const makeTextBold = pipe(addClass('font-bold'));
+  const makeTextThin = pipe(removeClass('font-bold'));
+  const renderLayout = pipe(setInnerHTML(getIssueTpl()));
+  const renderOpenedIssues = pipe(
+    setInnerHTML(openedIssues.map(getIssueItemTpl).join(''))
+  );
+  const renderClosedIssues = pipe(
+    setInnerHTML(closedIssues.map(getIssueItemTpl).join(''))
+  );
+  const renderOpenedButton = pipe(
+    setInnerText(`${openedIssues.length} Opened`),
+    setEvent('click', () => {
+      makeTextBold($(sel.openedButton));
+      makeTextThin($(sel.closedButton));
+      renderOpenedIssues($(sel.issueList));
+    })
+  );
+  const renderClosedButton = pipe(
+    setInnerText(`${closedIssues.length} Closed`),
+    setEvent('click', () => {
+      makeTextBold($(sel.closedButton));
+      makeTextThin($(sel.openedButton));
+      renderClosedIssues($(sel.issueList));
+    })
+  );
+
+  function render() {
+    renderLayout($(sel.app));
     renderOpenedIssues($(sel.issueList));
-  })
-);
-const renderClosedButton = pipe(
-  setInnerText(`${closedIssues.length} Closed`),
-  setEvent('click', () => {
-    makeTextBold($(sel.closedButton));
-    makeTextThin($(sel.openedButton));
-    renderClosedIssues($(sel.issueList));
-  })
-);
+    renderOpenedButton($(sel.openedButton));
+    renderClosedButton($(sel.closedButton));
+  }
 
-function render() {
-  renderLayout($(sel.app));
-  renderOpenedIssues($(sel.issueList));
-  renderOpenedButton($(sel.openedButton));
-  renderClosedButton($(sel.closedButton));
+  return { render };
 }
 
-window.addEventListener('DOMContentLoaded', render);
+const app = await App();
+window.addEventListener('DOMContentLoaded', app.render());
