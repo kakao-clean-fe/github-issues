87016b149bc787e8998e7905f025d8796ec2fead
Sotaneum <gnyontu39@gmail.com>
2022-11-07 18:41:02 +0900

    [WEEK-1] pipe를 좀 더 사용해보자

--- src/main.js
+++ src/main.js
@@ -1,102 +1,83 @@
 import { getIssueItemTpl, getIssueTpl } from "./tpl";
-
 const DATA_SOURCE_ISSUES = "/data-sources/issues.json";
 
-const domParser = new DOMParser();
-
-const pipeStack = (funcs, value) =>
-  funcs.reduce((acc, func) => ({ ...acc, ...func(acc) }), { value });
+const suffix = {
+  open: "Opens",
+  close: "Closed",
+};
 
 const pipe = (funcs, value) => funcs.reduce((acc, func) => func(acc), value);
-
-const parser = (text) => ({
-  target: domParser.parseFromString(text, "text/html").body.firstChild,
-});
-
-const toDict =
-  (getKey = (item) => !item) =>
-  (items = []) =>
-    items.reduce((acc, item) => {
-      const key = getKey(item);
-      return { ...acc, [key]: [...(acc[key] || []), item] };
-    }, {});
-
 const toFetch = (path, callback = (items) => items) =>
   fetch(path)
     .then((res) => res.json())
     .then(callback);
 
-const selector = (elementDict = {}, select) => {
-  Object.values(elementDict).forEach((element) =>
-    element.classList.remove("font-bold")
-  );
-  elementDict[select].classList.add("font-bold");
-};
+const parser = new DOMParser();
 
-const toIssueData = toDict((item) => item.status);
+const findById = (id) => document.getElementById(id);
 
-const appendItems = (target) => (items) => {
+const toHtml = (text) =>
+  parser.parseFromString(text, "text/html").body.firstChild;
+
+const toType = (el) => (el.classList.contains("open-count") ? "open" : "close");
+
+const renderBody = (bodyTemplate) => (target) =>
+  pipe([bodyTemplate, toHtml, (html) => target.appendChild(html)]);
+
+const filterStatus = (items) => (status) =>
+  items.filter((item) => item.status === status);
+
+const renderData = (target) => (status) => (itemTemplate) => (items) => {
   target.innerText = "";
-  items.forEach((item) => target.appendChild(item));
+  filterStatus(items)(status)
+    .map(itemTemplate)
+    .map(toHtml)
+    .forEach((el) => target.appendChild(el));
+  return items;
 };
 
-const renderItems =
-  (itemCreator) =>
-  (items = []) =>
-    items.map((item) => pipe([itemCreator, parser], item).target);
+const changeStatus = (target) => (items) => (status) => {
+  const type = toType(target);
+  target.classList.toggle("font-bold", status === type);
+  target.innerText = `${filterStatus(items)(type).length} ${suffix[type]}`;
+};
 
-const renderIssueItems =
-  (items) =>
-  ({ ul, count, value }) =>
-    pipeStack(
+const updateStatus = (target) => (status) => (items) => {
+  [...target.querySelectorAll("div")].forEach((el) =>
+    changeStatus(el)(items)(status)
+  );
+  return items;
+};
+
+const addEventStatus = (body) => {
+  const statusTab = body.querySelector(".statusTab");
+  [...statusTab.querySelectorAll("div")].forEach((el) => {
+    el.addEventListener("click", () => pipe([toType, fetchData], el)(body));
+  });
+  return body;
+};
+
+const fetchData = (status) => (body) => {
+  const ul = body.querySelector("ul");
+  const statusTab = body.querySelector(".statusTab");
+
+  toFetch(DATA_SOURCE_ISSUES).then((items) =>
+    pipe(
       [
-        (data) => ({ issueData: toIssueData(data.value) }),
-        ({ issueData }) => {
-          selector(count, value);
-          count.open.innerText = `${issueData["open"].length} Opens`;
-          count.close.innerText = `${issueData["close"].length} Closed`;
-          return {
-            issueAppend: appendItems(ul),
-            createItems: renderItems(getIssueItemTpl),
-          };
-        },
-        ({ issueData, createItems, issueAppend }) =>
-          pipe([createItems, issueAppend], issueData[value]),
+        renderData(ul)(status)(getIssueItemTpl),
+        updateStatus(statusTab)(status),
       ],
       items
-    );
-
-const loadItems = (data) =>
-  toFetch(DATA_SOURCE_ISSUES, renderIssueItems).then((callback) =>
-    callback(data)
+    )
   );
+  return body;
+};
 
-const render = (app) =>
-  pipeStack(
-    [
-      () => pipe([parser], getIssueTpl()),
-      ({ target }) => {
-        app.appendChild(target);
-
-        return {
-          ul: target.querySelector("ul"),
-          count: {
-            open: target.querySelector(".open-count"),
-            close: target.querySelector(".close-count"),
-          },
-        };
-      },
-      ({ count, ...data }) => {
-        count.open.addEventListener("click", () =>
-          loadItems({ ...data, count, value: "open" })
-        );
-        count.close.addEventListener("click", () =>
-          loadItems({ ...data, count, value: "close" })
-        );
-      },
-      loadItems,
-    ],
-    "open"
+const render = (status = "open") => {
+  pipe(
+    [findById, renderBody(getIssueTpl), fetchData(status), addEventStatus],
+    "app"
   );
+};
 
-render(document.getElementById("app"));
+render();
