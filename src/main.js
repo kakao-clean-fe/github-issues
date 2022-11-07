f2a729727f8c83d0f7aad00d4813a0dd0339f636
simon-kim <simon.kim@kakaocorp.com>
2022-11-07 15:38:15 +0900

    fix: refector render

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { GlobalStore } from "./store.js";
-import {
-  getIssueTpl,
-  getIssueItemTpl,
-} from "./tpl.js";
+import { getIssueTpl } from "./tpl.js";
 import { getIssuesData } from "./api.js";
+import { renderFilterList, renderList } from "./render.js";
+import {
+  CLASS_APP,
+  CLASS_CLOSE_BTN,
+  CLASS_OPEN_BTN,
+  KEY_CLOSE,
+  KEY_OPEN,
+} from "./constants.js";
 
 const store = GlobalStore();
 
@@ -11,62 +16,37 @@ const init = async () => {
   // localStorage 초기화
   store.initStore();
   // 템플릿 그리기
-  document.querySelector("#app").innerHTML = getIssueTpl();
+  document.querySelector(CLASS_APP).innerHTML = getIssueTpl();
   // 데이터 조회
   await getIssuesData(store);
   // 이벤트 핸들러 구현
   addEventHandler();
   // 데이터 렌더링
-  renderList();
+  const issues = store.getStore().issues;
+  renderList(issues);
 };
 
 const addEventHandler = () => {
   // opens 클릭 이벤트 구현
-  document.querySelector(".open-count").addEventListener("click", evtOpenCountClick);
+  document
+    .querySelector(CLASS_OPEN_BTN)
+    .addEventListener("click", evtOpenCountClick);
   // closed 클릭 이벤트 구현
-  document.querySelector(".close-count").addEventListener("click", evtCloseCountClick);
+  document
+    .querySelector(CLASS_CLOSE_BTN)
+    .addEventListener("click", evtCloseCountClick);
 };
 
 const evtOpenCountClick = () => {
-  renderFilterList('open');  
+  const issues = store.getStore().issues;
+  if (!issues) return;
+  renderFilterList(KEY_OPEN, issues);
 };
 
 const evtCloseCountClick = () => {
-  renderFilterList('close');  
-};
-
-const renderFilterList = (key) => {
-  const items = store.getStore().issues;
-
-  document.querySelector(".issue-list-ul").innerHTML = '';
-  switch (key) {
-    case 'open':
-      const openItems = items.filter((issue) => issue.status === "open");
-      openItems.map((issue) => {
-        document.querySelector(".issue-list-ul").innerHTML += getIssueItemTpl(issue);
-      });
-      break;
-    case 'close':
-      const closeItems = items.filter((issue) => issue.status === "close");
-      closeItems.map((issue) => {
-        document.querySelector(".issue-list-ul").innerHTML += getIssueItemTpl(issue);
-      });
-      break; 
-  }
-};
-
-const renderList = () => {
-  const items = store.getStore().issues;
-
-  const openCnt = items.filter((issue) => issue.status === "open").length;
-  const closeCnt = items.filter((issue) => issue.status === "close").length;
-
-  document.querySelector(".open-count-value").innerHTML = openCnt;
-  document.querySelector(".close-count-value").innerHTML = closeCnt;
-
-  items.map((issue) => {
-    document.querySelector(".issue-list-ul").innerHTML += getIssueItemTpl(issue);
-  });
+  const issues = store.getStore().issues;
+  if (!issues) return;
+  renderFilterList(KEY_CLOSE, issues);
 };
 
 export default function App() {
