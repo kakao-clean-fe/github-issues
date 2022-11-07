620872e5ab0ae12384e936c4e581f5d41235eda5
yejineee <yygeniee@gmail.com>
2022-11-07 19:31:50 +0900

    Feat: reduceArray 함수 작성

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -5,3 +5,7 @@ export const filterArray = <T>({ arr, filterFunc }: { arr: T[], filterFunc: Para
 export const countArray = ({ arr }: { arr: unknown[] }): number => {
   return arr.length;
 };
+
+export const reduceArray = <ReturnValue>({ arr, initValue, reducer }: { arr: unknown[], initValue: unknown, reducer: Parameters<typeof Array.prototype.reduce>[0] }): ReturnValue => {
+  return arr.reduce(reducer, initValue) as ReturnValue;
+};
