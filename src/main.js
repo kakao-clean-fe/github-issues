bf6de4852d2799cda3d2b281133eb0fee2af07b4
jordan.song <jordan.song@kakaocorp.com>
2022-11-07 17:35:18 +0900

    WEEK-1 렌더 함수 리팩토리

--- src/main.js
+++ src/main.js
@@ -11,37 +11,37 @@ import {
   removeClass,
 } from './curry/dom';
 
-async function render() {
-  const issues = await fetchIssues();
-  const openedIssues = issues.filter(isOpenedIssue);
-  const closedIssues = issues.filter(isClosedIssue);
+const issues = await fetchIssues();
+const openedIssues = issues.filter(isOpenedIssue);
+const closedIssues = issues.filter(isClosedIssue);
 
-  const makeTextBold = pipe(addClass('font-bold'));
-  const makeTextThin = pipe(removeClass('font-bold'));
-  const renderLayout = pipe(setInnerHTML(getIssueTpl()));
-  const renderOpenedIssues = pipe(
-    setInnerHTML(openedIssues.map(getIssueItemTpl).join(''))
-  );
-  const renderClosedIssues = pipe(
-    setInnerHTML(closedIssues.map(getIssueItemTpl).join(''))
-  );
-  const renderOpenedButton = pipe(
-    setInnerText(`${openedIssues.length} Opened`),
-    setEvent('click', () => {
-      makeTextBold($(sel.openedButton));
-      makeTextThin($(sel.closedButton));
-      renderOpenedIssues($(sel.issueList));
-    })
-  );
-  const renderClosedButton = pipe(
-    setInnerText(`${closedIssues.length} Closed`),
-    setEvent('click', () => {
-      makeTextBold($(sel.closedButton));
-      makeTextThin($(sel.openedButton));
-      renderClosedIssues($(sel.issueList));
-    })
-  );
+const makeTextBold = pipe(addClass('font-bold'));
+const makeTextThin = pipe(removeClass('font-bold'));
+const renderLayout = pipe(setInnerHTML(getIssueTpl()));
+const renderOpenedIssues = pipe(
+  setInnerHTML(openedIssues.map(getIssueItemTpl).join(''))
+);
+const renderClosedIssues = pipe(
+  setInnerHTML(closedIssues.map(getIssueItemTpl).join(''))
+);
+const renderOpenedButton = pipe(
+  setInnerText(`${openedIssues.length} Opened`),
+  setEvent('click', () => {
+    makeTextBold($(sel.openedButton));
+    makeTextThin($(sel.closedButton));
+    renderOpenedIssues($(sel.issueList));
+  })
+);
+const renderClosedButton = pipe(
+  setInnerText(`${closedIssues.length} Closed`),
+  setEvent('click', () => {
+    makeTextBold($(sel.closedButton));
+    makeTextThin($(sel.openedButton));
+    renderClosedIssues($(sel.issueList));
+  })
+);
 
+function render() {
   renderLayout($(sel.app));
   renderOpenedIssues($(sel.issueList));
   renderOpenedButton($(sel.openedButton));
