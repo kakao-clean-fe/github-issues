051a21b9ccacd24eaf43d47ef0ea3a342c38df3e
yejineee <yygeniee@gmail.com>
2022-11-07 14:19:32 +0900

    Feat: issue list 받아와서 그려주기

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import { getIssueTpl } from '~/tpl.js';
+import { getIssueTpl, getIssueItemTpl } from '~/tpl.js';
+import { getIssues } from '~/store/action';
 
 document.querySelector('#app').innerHTML = getIssueTpl();
+
+const renderIssueList = async () => {
+  const issues = await getIssues();
+  const issuesTemplate = issues.reduce((template, issue) => {
+    return template + getIssueItemTpl(issue);
+  }, '');
+
+  document.querySelector('.issue-list > ul').innerHTML = issuesTemplate;
+};
+
+renderIssueList();
