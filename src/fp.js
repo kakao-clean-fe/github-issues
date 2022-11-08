724f31f9dd3c93263b0d794b1fa7e8bfb8232bbb
berry.straww <berry.straww@kakaocorp.com>
2022-11-08 11:44:43 +0900

    구현 완료

---
+++ src/fp.js
@@ -0,0 +1,32 @@
+export const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+export const map = curry((f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+});
+
+export const filter = curry((f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    if (f(a)) res.push(a);
+  }
+  return res;
+});
+
+export const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+});
+
+export const go = (_, ...fs) => reduce((_, f) => f(_), _, fs);
+
+export const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
