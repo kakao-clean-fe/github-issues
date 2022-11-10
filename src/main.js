31b2923541df59c314af106889550aff7cd52d2f
minkky <mona.me@kakaocorp.com>
2022-11-10 14:53:57 +0900

    refactor: main async 추가

--- src/main.js
+++ src/main.js
@@ -2,21 +2,23 @@ import { getIssueTpl, getIssueItemTpl } from "./template";
 import { getData } from "./api";
 import { issueUtils } from "./utils";
 
-const issueData = await getData("issues");
-const opens = issueUtils.getIssueWith(issueData, "open");
-const closed = issueUtils.getIssueWith(issueData, "close");
+(async () => {
+  const issueData = await getData("issues");
+  const opens = issueUtils.getIssueWith(issueData, "open");
+  const closed = issueUtils.getIssueWith(issueData, "close");
 
-const app = document.querySelector("#app");
-app.innerHTML = getIssueTpl(opens.length, closed.length);
+  const app = document.querySelector("#app");
+  app.innerHTML = getIssueTpl(opens.length, closed.length);
 
-const openCloseTab = [
-  document.querySelector(".open-count"),
-  document.querySelector(".close-count"),
-];
-openCloseTab.forEach((tab, idx) => {
-  tab.addEventListener("click", () => {
-    issueUtils.toggleOpenCloseTab(issueData, openCloseTab, idx === 0);
+  const openCloseTab = [
+    document.querySelector(".open-count"),
+    document.querySelector(".close-count"),
+  ];
+  openCloseTab.forEach((tab, idx) => {
+    tab.addEventListener("click", () => {
+      issueUtils.toggleOpenCloseTab(issueData, openCloseTab, idx === 0);
+    });
   });
-});
 
-issueUtils.setIssueItems(opens);
+  issueUtils.setIssueItems(opens);
+})();
