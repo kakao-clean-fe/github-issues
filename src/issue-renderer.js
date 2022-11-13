5672d32185fecca0d36a8a9322e3e1a9447bcf68
nina.oh <nina.oh@kakaocorp.com>
2022-11-14 02:33:04 +0900

    새 페이지 작업을 위한 기존 파일 이동

--- src/issue-renderer.js
+++ src/issue-renderer.js
@@ -1,16 +1,18 @@
 import {getIssueItemTpl, getIssueTpl} from "./tpl.js";
-import {pipe, tab} from "./fp-helpers.js";
+import {pipe, tab} from "./helpers/fp-helpers.js";
 
-document.getElementById('app').innerHTML = getIssueTpl();
-export const openedButtonElement = document.getElementsByClassName('open-count')[0];
-export const closedButtonElement = document.getElementsByClassName('close-count')[0];
-const issuesListElement = document.querySelector('.issue-list ul');
-const htmlToElement = pipe(
+export const htmlToElement = pipe(
   html => html.trim(),
   (html) => ({ele: document.createElement('template'), html}),
   tab(({ele, html}) => ele.innerHTML = html),
   ({ele}) => ele.content.firstChild
 );
+
+document.getElementById('app').innerHTML = getIssueTpl();
+export const openedButtonElement = document.getElementsByClassName('open-count')[0];
+export const closedButtonElement = document.getElementsByClassName('close-count')[0];
+const issuesListElement = document.querySelector('.issue-list ul');
+
 const appendIssueElement = (issueElement) => issuesListElement.appendChild(issueElement);
 export const renderIssuesClear = () => issuesListElement.innerHTML = '';
 export const renderOpenedNum = (openedNum) => openedButtonElement.innerText = openedNum + ' Opened';
