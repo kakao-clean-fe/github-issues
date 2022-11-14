f31154451167582218d2b7d322a538ba7249470b
nina.oh <nina.oh@kakaocorp.com>
2022-11-14 23:08:59 +0900

    라벨 추가 로직

--- src/label-page.js
+++ src/label-page.js
@@ -5,18 +5,17 @@ import {htmlToElement} from "./issue-renderer.js";
 import {pipe} from "./helpers/fp-helpers.js";
 
 class LabelStore {
-  labelList = [];
-  color = '';
+  _labelList = [];
   constructor() {
     fetchLabels().then((labels) => {
       this.labelList = labels;
+      console.log(labels)
     });
   }
-  createLabel() {
-    console.log('createLabel');
-  }
-  generateRandomColor() {
-    console.log('generateRandomColor');
+  createLabel(label) {
+    console.log(this.labelList)
+    this.labelList = this._labelList.concat([label]);
+    console.log('createLabel', label);
   }
 }
 
@@ -24,11 +23,54 @@ class LabelCreateView {
   clickCreateBtn$ = new Observable();
   clickColorGenButton$ = new Observable();
   isShowCreateView = false;
+  // 얘네도 다 모델로 빼야 할까..?
+  labelNameValue = '';
+  descriptionValue = '';
+  color = '';
+  // form = {
+  //   labelNameValue: '',
+  //   descriptionValue: '',
+  //   color: '',
+  // };
+
   constructor() {
-    document.getElementById('new-label-button').addEventListener('click', this.toggleCreateView)
+    document.getElementById('new-label-button').addEventListener('click', this.toggleCreateView.bind(this));
+    document.getElementById('new-label-color').addEventListener('click', this.refreshColor.bind(this));
+    document.getElementById('label-create-button').addEventListener('click', this.onClickCreateButton.bind(this))
+
+    const labelNameInputElement = document.getElementById('label-name-input');
+    labelNameInputElement.addEventListener('input', () => {
+      this.labelNameValue = labelNameInputElement.value;
+      this.renderLabelPreview();
+      this.activeCreateButton();
+    });
+    const labelDescriptionInputElement = document.getElementById('label-description-input');
+    labelDescriptionInputElement.addEventListener('input', () => {
+      this.descriptionValue = labelDescriptionInputElement.value;
+      this.activeCreateButton();
+    });
+  }
+
+  refreshColor() {
+    this.generateRandomColor();
+    this.renderColor();
+    this.renderLabelPreview();
+
+    this.activeCreateButton();
+  }
+
+  generateRandomColor() {
+    const randomRGBValue = () => Math.floor(Math.random() * 256 * 256 * 256);
+    this.color = randomRGBValue().toString(16);
   }
   renderColor() {
-    console.log('renderColor');
+    document.getElementById('new-label-color').style.backgroundColor = '#' + this.color;
+    document.getElementById('label-color-value').value = this.color;
+  }
+  renderLabelPreview() {
+    const labelPreviewElement = document.getElementById('label-preview');
+    labelPreviewElement.style.backgroundColor = '#' + this.color;
+    labelPreviewElement.innerText = this.labelNameValue ? this.labelNameValue : 'Label Preview';
   }
   toggleCreateView() {
     if (!this.isShowCreateView) {
@@ -39,6 +81,25 @@ class LabelCreateView {
       this.isShowCreateView = false;
     }
   }
+  onClickCreateButton(e) {
+    e.preventDefault()
+
+    if (this.labelNameValue && this.descriptionValue && this.color) {
+      this.clickCreateBtn$.next({
+        name: this.labelNameValue,
+        description: this.descriptionValue,
+        color: this.color,
+      })
+    }
+  }
+  activeCreateButton() {
+    if (this.labelNameValue && this.descriptionValue && this.color) {
+      document.getElementById('label-create-button').disabled = false;
+    } else {
+      document.getElementById('label-create-button').disabled = true;
+    }
+
+  }
 }
 
 class LabelListView {
@@ -46,6 +107,9 @@ class LabelListView {
     document.getElementById('app').innerHTML = getLabelTpl();
   }
   render(labels) {
+    console.log('render', labels);
+    const labelsListElement = document.querySelector('ul.label-list');
+    labelsListElement.innerHTML = '';
     labels.forEach(this._renderLabel);
   }
   _renderLabel(label) {
@@ -63,12 +127,12 @@ export function initLabelPage() {
   const labelListView = new LabelListView();
   const labelCreateView = new LabelCreateView();
 
-  labelCreateView.clickCreateBtn$.subscribe(() => {
-    labelStore.createLabel();
+  labelCreateView.clickCreateBtn$.subscribe((label) => {
+    labelStore.createLabel(label);
   });
 
   labelCreateView.clickColorGenButton$.subscribe(() => {
-    labelStore.generateRandomColor();
+    // labelStore.generateRandomColor();
   })
 
   Object.defineProperty(labelStore, 'labelList', {
@@ -76,15 +140,8 @@ export function initLabelPage() {
     },
     set(labels) {
       labelListView.render(labels);
+      this._labelList = labels;
     }
   });
-
-  Object.defineProperty(labelStore, 'color', {
-    get() {
-    },
-    set() {
-      labelCreateView.renderColor(this.color);
-    }
-  })
 }
 
--- src/tpl.js
+++ src/tpl.js
@@ -191,7 +191,7 @@ export function getLabelTpl() {
         class="form-group my-2 flex mt-10">
         <button type="button" class="base-outer p-2 mr-4"> Cancel
         </button>
-        <button id="label-create-button" type="submit" class="base-outer p-2 mr-4 bg-green-700 opacity-50 text-white" disabled=""> Create label
+        <button id="label-create-button"  class="base-outer p-2 mr-4 bg-green-700 opacity-50 text-white" disabled=""> Create label
         </button>
       </div>
       <!--END new label actions-->
