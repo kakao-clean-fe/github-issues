e315e5cb477600c733efe791898528d541f17ce4
YooDonggwan <dbehdrhks@naver.com>
2022-11-15 08:42:37 +0900

    remove: 미사용 코드 삭제

--- src/views/LabelCreator.js
+++ src/views/LabelCreator.js
@@ -3,12 +3,6 @@ import { selectElement } from '../utils/dom';
 import { SELECTOR } from '../constants/selector';
 
 export const LabelCreator = class {
-
-  get events () {
-
-  }
-
-  // 데이터 초기화 -> 템플릿 생성 -> 이벤트 등록
   get template () {
     return getLabelCreatorTpl();
   }
@@ -27,7 +21,6 @@ export const LabelCreator = class {
   initTemplate() {
     if (!this.element) { return; }
 
-    // 템플릿 생성
     this.element.innerHTML = this.template;
 
     // TODO: 이벤트 등록 필요한 요소 찾아서 이벤트 등록
