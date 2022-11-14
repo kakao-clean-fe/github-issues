2990310123adc320d0d096d2095d60738071dcb1
jake.kyo <jake.kyo@kakaocorp.com>
2022-11-14 12:08:27 +0900

    1주차 PR 리뷰 반영

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { MainPage } from './components/page/mainPage';
 import { IssueComponent } from './components/page/item/issue';
 
 class App {
+  constructor(appRoot) {
+    this.loadPage(appRoot);
+  }
+
   #page = '';
   #rawIssues = [];
   #issueListElement = null;
@@ -10,11 +14,40 @@ class App {
     close: null,
   };
 
+  loadPage = (appRoot) => {
+    this.fetchIssues().then((issues) => {
+      this.#rawIssues = issues;
+
+      // 메인 페이지 렌더링
+      this.#page = new MainPage(
+        this.filterIssues('open').length,
+        this.filterIssues('close').length
+      );
+      this.#page.attatchTo(appRoot);
+
+      // 이슈 리스트 렌더링
+      this.#issueListElement = document.querySelector('.issue-list ul');
+      this.renderIssues('open'); // 디폴트는 open 상태
+
+      // 이벤트 등록
+      this.#tabs.open = document.getElementById('openTab');
+      this.#tabs.close = document.getElementById('closedTab');
+      this.#tabs.open.addEventListener('click', () => {
+        this.renderIssues('open');
+        this.changeTab('open');
+      });
+      this.#tabs.close.addEventListener('click', () => {
+        this.renderIssues('close');
+        this.changeTab('close');
+      });
+    });
+  };
+
   fetchIssues = () => {
     // 반환값을 받을 Promise 객체 생성
     return new Promise(function (resolve) {
       fetch('../../data-sources/issues.json') // json 파일 읽어오기
-        .then(function (response) {
+        .then((response) => {
           resolve(response.json()); // 읽어온 데이터를 json으로 변환
         });
     });
@@ -47,45 +80,16 @@ class App {
     }
   };
 
-  changeTab = (status) => {
+  changeTab = (clickedStatus) => {
     // 클릭한 탭과 일치하면 하이라이팅, 아니면 제거
-    Object.keys(this.#tabs).forEach((key) => {
-      if (key === status) {
-        this.#tabs[key].classList.add('font-bold');
+    Object.keys(this.#tabs).forEach((issueStatus) => {
+      if (issueStatus === clickedStatus) {
+        this.#tabs[issueStatus].classList.add('font-bold');
       } else {
-        this.#tabs[key].classList.remove('font-bold');
+        this.#tabs[issueStatus].classList.remove('font-bold');
       }
     });
   };
-
-  constructor(appRoot) {
-    this.fetchIssues().then((issues) => {
-      this.#rawIssues = issues;
-
-      // 메인 페이지 렌더링
-      this.#page = new MainPage(
-        this.filterIssues('open').length,
-        this.filterIssues('close').length
-      );
-      this.#page.attatchTo(appRoot);
-
-      // 이슈 리스트 렌더링
-      this.#issueListElement = document.querySelector('.issue-list ul');
-      this.renderIssues('open'); // 디폴트는 open 상태
-
-      // 이벤트 등록
-      this.#tabs.open = document.getElementById('openTab');
-      this.#tabs.close = document.getElementById('closedTab');
-      this.#tabs.open.addEventListener('click', () => {
-        this.renderIssues('open');
-        this.changeTab('open');
-      });
-      this.#tabs.close.addEventListener('click', () => {
-        this.renderIssues('close');
-        this.changeTab('close');
-      });
-    });
-  }
 }
 
 window.onload = () => {
