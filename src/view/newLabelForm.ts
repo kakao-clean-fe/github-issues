d622944c55d6048bed232d77facc0524fba2fad8
SeonJun-Hwang <seonjun-hwang@github.com>
2022-11-14 16:58:08 +0900

    fix: labelform 이벤트 분리
    arrow function에서 class method로 변경

--- src/view/newLabelForm.ts
+++ src/view/newLabelForm.ts
@@ -12,12 +12,12 @@ export default class NewLabelForm {
     private labelPresenter: LabelPresenterType;
     private readonly $form: HTMLFormElement | null;
     private readonly $labelPreview: HTMLSpanElement | null = null;
-    private $labelInput: HTMLInputElement | null = null;
-    private $labelDescription: HTMLInputElement | null = null;
+    private readonly $labelInput: HTMLInputElement | null = null;
+    private readonly $labelDescription: HTMLInputElement | null = null;
     private readonly $labelRandomColor: HTMLButtonElement | null = null;
     private readonly $labelColorCode: HTMLInputElement | null = null;
-    private $createButton: HTMLButtonElement | null = null;
-    private labelState: LabelState = { name: '', desc: '', color: '' };
+    private readonly $createButton: HTMLButtonElement | null = null;
+    private labelState: LabelState = {name: '', desc: '', color: ''};
 
     public constructor(presenter: LabelPresenterType) {
         this.labelPresenter = presenter;
@@ -36,39 +36,51 @@ export default class NewLabelForm {
     }
 
     private attachEvent() {
-        this.$labelRandomColor?.addEventListener('click', () => {
-            const randomColor = Math.round(Math.random() * 0xffffff);
-            const hexCode = `#${randomColor.toString(16).toUpperCase()}`;
-            this.setState({ color: hexCode });
-            if (this.$labelColorCode) this.$labelColorCode.value = hexCode;
-            if (this.$labelRandomColor) this.$labelRandomColor.style.backgroundColor = hexCode;
-            if (this.$labelPreview) this.$labelPreview.style.backgroundColor = hexCode;
-        });
-        this.$labelInput?.addEventListener('input', () => {
-            if (!this.$labelInput) return;
-            this.setState({ name: this.$labelInput.value.trim() });
-        });
-        this.$labelDescription?.addEventListener('input', () => {
-            if (!this.$labelDescription) return;
-            this.setState({ desc: this.$labelDescription.value.trim() });
-        })
-        this.$form?.addEventListener('submit', (e) => {
-            const newLabel: Label = { name: this.labelState.name, description: this.labelState.desc, color: this.labelState.color.replace('#', '') };
-            this.labelPresenter.appendLabelList(newLabel);
-            e.preventDefault();
-        })
+        this.$labelRandomColor?.addEventListener('click', this.updateRandomColorCode.bind(this));
+        this.$labelInput?.addEventListener('input', this.updateLabelInput.bind((this)));
+        this.$labelDescription?.addEventListener('input', this.updateLabelDesc.bind(this));
+        this.$form?.addEventListener('submit', this.submitForm.bind(this));
     }
 
     private setState(nextState: Partial<LabelState>) {
-        this.labelState = { ...this.labelState, ...nextState };
+        this.labelState = {...this.labelState, ...nextState};
         this.updateCreateButton();
+    }
+
+    private updateRandomColorCode() {
+        const randomColor = Math.round(Math.random() * 0xffffff);
+        const hexCode = `#${randomColor.toString(16).toUpperCase()}`;
+        this.setState({color: hexCode});
+        if (this.$labelColorCode) this.$labelColorCode.value = hexCode;
+        if (this.$labelRandomColor) this.$labelRandomColor.style.backgroundColor = hexCode;
+        if (this.$labelPreview) this.$labelPreview.style.backgroundColor = hexCode;
+    }
+
+    private updateLabelInput() {
+        if (!this.$labelInput) return;
+        this.setState({name: this.$labelInput.value.trim()});
+    }
+
+    private updateLabelDesc() {
+        if (!this.$labelDescription) return;
+        this.setState({desc: this.$labelDescription.value.trim()});
+    }
+
+    private submitForm(e: SubmitEvent) {
+        const newLabel: Label = {
+            name: this.labelState.name,
+            description: this.labelState.desc,
+            color: this.labelState.color.replace('#', '')
+        };
+        this.labelPresenter.appendLabelList(newLabel);
+        e.preventDefault();
     }
 
     private validation() {
         return !!this.labelState.name && !!this.labelState.desc && !!this.labelState.color;
     }
 
-    private updateCreateButton () {
+    private updateCreateButton() {
         if (!this.$createButton) return;
         this.$createButton.disabled = !this.validation();
         if (this.$createButton.disabled) this.$createButton.classList.add('opacity-50');
