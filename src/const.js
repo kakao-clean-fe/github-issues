885d88033d70b17d61a054950e7d6f21db5f7b16
roddy.oh <roddy.oh@kakaocorp.com>
2022-11-15 11:09:03 +0900

    feat: Label 리스트 구현

--- src/const.js
+++ src/const.js
@@ -1,6 +1,11 @@
 export const OPEN = 'open';
 export const CLOSE = 'close';
 
-export const UL_SELECTOR = '#issues-wrapper > div.issue-list.flex.ml-auto > ul';
+export const ISSUE_SELECTOR = 'body > nav > button:nth-child(1)';
+export const LABEL_SELECTOR = 'body > nav > button:nth-child(2)';
+
+export const ISSUE_UL_SELECTOR = '#issues-wrapper > div.issue-list.flex.ml-auto > ul';
 export const OPEN_BUTTON_SELECTOR = '#issues-wrapper div.whitespace-nowrap.open-count.font-bold.cursor-pointer'
 export const CLOSE_BUTTON_SELECTOR = '#issues-wrapper div.whitespace-nowrap.close-count.ml-3.cursor-pointer'
+
+export const LABEL_UL_SELECTOR = '#labels-wrapper > ul';
