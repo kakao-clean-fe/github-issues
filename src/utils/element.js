166be22d8ed9c2e05b83f1a0b3c58000b502e0f1
wes-lee <wes.lee@kakaocorp.com>
2022-11-15 22:34:16 +0900

    Feat: clearInnerHTML 유틸 함수 추가

--- src/utils/element.js
+++ src/utils/element.js
@@ -1,4 +1,5 @@
 export const getElement = (selector) => document.querySelector(selector);
+export const clearInnerHTML = (element) => element.innerHTML = '';
 export const convertTemplateToElement = (template) => {
   const $div = document.createElement('div');
   $div.innerHTML = template.trim();
