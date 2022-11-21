caf165f10c5c81ce00f557c53a884449a61f1bf7
wes-lee <wes.lee@kakaocorp.com>
2022-11-21 18:48:06 +0900

    Feat: Router 컴포넌트 추가

---
+++ src/core/router.ts
@@ -0,0 +1,30 @@
+interface Page {
+  path: string;
+  file: string;
+}
+
+export default class Router {
+  path: string;
+  page: Page;
+
+  constructor (){
+    this.path = window.location.pathname;
+    this.page = this.pages.filter(page => page.path === this.path)[0];
+
+    this.importPage(this.page.file);
+  }
+
+  // 메인 페이지에서만 정의해야한다.
+  get pages(): Page[] {
+    return [];
+  }
+
+  private importPage (fileName) {
+    const PAGE_PREFIX = '../page';
+    const url = `${PAGE_PREFIX}/page-${fileName}.js`;
+
+    import(url).then((module) => {
+      module.render();
+    })
+  }
+}
