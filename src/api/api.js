c1e5dc796254dd26f814eace3aaab95fba720a8b
minkky <mona.me@kakaocorp.com>
2022-11-21 19:00:04 +0900

    feat: api error 인 경우 처리

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,21 @@
+const handleError = (err) => {
+  console.warn(err);
+  return new Response(
+    JSON.stringify({
+      code: 400,
+      message: "잠시 뒤 다시 시도해주세요",
+    })
+  );
+};
+
 const getData = async (fileName) => {
-  const jsonData = await fetch(`/${fileName}`);
-  return await jsonData.json();
+  const res = await fetch(`/${fileName}`).catch(handleError);
+  console.log("res", res);
+  if (res.ok) {
+    return res.json();
+  } else {
+    alert("잠시 뒤 다시 시도해주세요");
+  }
 };
 
 export { getData };
