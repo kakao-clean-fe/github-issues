51f6f890cc4986105b680e9b02d29b8740a67bb4
ch1517 <doqtqu@gmail.com>
2022-11-21 18:24:17 +0900

    [Step3] Fix : MSW 요청 핸들러에 나열되지 않는 요청을 처리하는 방법으로 warning 메세지 해결

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { labelPageStr } from "./constants/template-label";
 import LabelPage from "./pages/label";
 import { worker } from './mocks/browser';
 
-worker.start();
+worker.start({
+  onUnhandledRequest: 'bypass',
+});
 
 const labelList = await getDataFromMSW('/labels');
 new LabelPage(labelPageStr, '#app', labelList);
