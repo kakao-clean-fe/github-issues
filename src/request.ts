17985f4b9e28c9594459b431b8bcdccf875df4f3
SeonJun-Hwang <seonjun-hwang@github.com>
2022-11-22 15:47:59 +0900

    fix: request에 signal을 추가할 수 있도록 변경

--- src/request.ts
+++ src/request.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 import { Issue, Label } from './types';
 
-const request = <T> (url: string) => async () => {
-    const { data } = await axios.get<T>(url, { responseType: 'json' });
+export type DelayRequestOption = {
+    signal?: AbortSignal
+}
+
+const GET = <T> (url: string) => async ({ signal }: DelayRequestOption = { signal: undefined }) => {
+    const { data } = await axios.get<T>(url, { responseType: 'json', signal });
     return data;
 }
 
-export const readIssues = request<Array<Issue>>('/data-sources/issues.json');
-export const readLabels = request<Array<Label>>('/data-sources/labels.json');
+const POST = <T = any> (url: string) => async (json: any) => {
+    const { data } = await axios.post<T>(url, json);
+    return data;
+}
+
+export const readIssues = GET<Array<Issue>>('/issues');
+export const readLabels = GET<Array<Label>>('/labels');
+export const readDelaiedLabels = GET<Array<Label>>('/labels-delay');
+export const updateLabels = POST('/labels');
