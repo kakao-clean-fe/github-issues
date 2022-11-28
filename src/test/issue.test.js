5925a25a49aa60f1fcb1b6cf77b4ff2ca3c182c8
ch1517 <doqtqu@gmail.com>
2022-11-28 21:14:40 +0900

    [Step4] Test : 테스트 실패 케이스 추가

--- src/test/issue.test.js
+++ src/test/issue.test.js
@@ -2,19 +2,54 @@
 // 2. issue 정보 불러와서 list에 추가
 // 3. open / close 버튼 동작
 
-import { getFetchData } from '../api';
+import {jest} from '@jest/globals';
+import { Window } from 'happy-dom';
 import { IssuesData } from '../stores/issue';
-import {jest} from '@jest/globals'
+import fetchMock from "jest-fetch-mock";
+import { getStatusCount } from '../utils/status';
+import { setDefaultTemplate } from '../utils/template';
+import { pipe } from '../utils/pipe';
+
+const mockData = [{
+  "title": "new issue",
+  "_id": 123,
+  "tags": [{
+      "tagName": "bug",
+      "color":"brown"
+  }, {
+      "tagName": "document",
+      "color":"blue"
+  }],
+  "status": "open",
+  "open-date": "6hours",
+  "creator": "crongro",
+  "projects": "",
+  "milestones": "sprint2",
+  "assignee": "crong",
+  "subtask": ["loream", "loreamlorem"],
+  "comments-count": 4
+}];
 
 describe('📄 [issue Page] :', () => {
   const issueData = IssuesData();
 
-  test('API로부터 가져온 issue 데이터 store에 저장 : ', async () => {
-    //given
-    const getIssueMock = jest.spyOn(issueData, 'setIssues');
-    const getIssues =  await getFetchData('issues');
-    issueData.setIssues(getIssues);
-    expect(getIssueMock).toBeCalledTimes(1);
-    expect(getIssueMock).not.toBe([]);
+  beforeEach(() => {
+    fetchMock.resetMocks();
   });
+
+  test('API로부터 가져온 issue 데이터를 store에 저장한다. ', () => {
+    issueData.setIssues(mockData);
+    expect(issueData.getIssues()[0]._id).toEqual(123);
+  });
+
+  test('store에 저장된 issue 데이터를 불러와 list에 추가한다. ', () => {
+
+    const window = new Window();
+    const document = window.document;
+    const issues = issueData.getIssues();
+    pipe(getStatusCount, setDefaultTemplate)(issues);
+    const header = document.querySelector('#header');
+    expect(header).not.toBe(undefined);
+  });
+
 });
